bf3ffed429411c6cf2030b96ec3a97893b01f1f5
Vhaefeli <vhaefeli@student.42lausanne.ch>
2022-12-06 09:20:43 +0100

    6 12 morning train

--- src/raycasting.c
+++ src/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: vhaefeli <vhaefeli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 15:50:05 by vhaefeli          #+#    #+#             */
-/*   Updated: 2022/12/05 18:04:17 by vhaefeli         ###   ########.fr       */
+/*   Updated: 2022/12/06 09:20:09 by vhaefeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,33 +38,70 @@ float	raycasting(t_data *data, t_img minimap)
 }
 void	draw_walls();
 
-int	ray_dir(float dir[2], float radius_angle)
+int	ray_dir(t_cam ray)
 {
-	float	x;
-	float	y;
-	float	angle_dir;
-
-	angle_dir = asin(dir[1]) + radius_angle;
-	x = cos(angle_dir);
-	y = sin(angle_dir);
-	if (x >= 0 && y >= 0)
+	if (ray.pos[0] >= 0 && ray.pos[1] >= 0)
 		return (1);
-	if (x < 0 && y >= 0)
+	if (ray.pos[0] < 0 && ray.pos[1] >= 0)
 		return (2);
-	if (x < 0 && y < 0)
+	if (ray.pos[0] < 0 && ray.pos[1] < 0)
 		return (3);
-	if (x >= 0 && y < 0)
+	if (ray.pos[0] >= 0 && ray.pos[1] < 0)
 		return (4);
 }
 
-float	dist_next_h(t_cam player, float radius_angle)
+t_cam	ini_ray(t_cam player, float radius_angle)
 {
-	float	dist;
-	int		dir_ray;
+	float	angle;
+	t_cam	ray;
 
-	dir_ray = ray_dir(player.dir, radius_angle);
+	angle = atan(player.dir[1] / player.dir[0]) + radius_angle;
+	ray.pos[0] = player.pos[0];
+	ray.pos[1] = player.pos[1];
+	ray.dir[0] = cos(angle);
+	ray.dir[1] = sin(angle);
+}
+
+float	dist_next_h(t_cam ray, int ray_dir)
+{
+	float	angle;
+
+	angle = atan(ray.dir[1] / ray.dir[0]); // a passer comme parametre pour eviter de calculer a chaque fois?
 	if (ray_dir > 2)
 	{
-		dist = (player.pos[1] - fabs(player.pos[1]) / ());
+		return (ray.pos[1] - fabs(ray.pos[1]) / sin(angle));
+	}
+	return (fabs(ray.pos[1]) + 1 - ray.pos[1] / sin(angle));
+}
+
+float	dist_next_v(t_cam player, float radius_angle)
+{
+	int		dir_ray;
+	float	angle;
+
+	dir_ray = ray_dir(player.dir, radius_angle);// a passer comme parametre pour eviter de calculer a chaque fois?
+	angle = atan(player.dir[1] / player.dir[0]) + radius_angle; // a passer comme parametre pour eviter de calculer a chaque fois?
+	if (ray_dir == 2 || ray_dir == 3)
+	{
+		return (player.pos[0] - fabs(player.pos[0]) / sin(angle));
+	}
+	return (fabs(player.pos[0]) + 1 - player.pos[0] / sin(angle));
+}
+
+int	check_wall(char **map, t_cam *raypoint, float radius_angle)
+{
+	float	dist_h;
+	float	dist_v;
+	int		abs_x;
+	int		abs_y;
+
+	abs_x = (int)(fabs(raypoint->pos[1]));
+	abs_y = (int)(fabs(raypoint->pos[0]));
+	dist_h = dist_next_h(*raypoint, radius_angle);
+	dist_v = dist_next_v(*raypoint, radius_angle);
+	if (dist_h <= dist_v)
+	{
+		if (map[abs_x - 1][abs_y] == 1);
+			raypoint.po
 	}
 }
