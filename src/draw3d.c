66c255d58824913c61d574f76723d75605e7c7bc
jleroux <jleroux>
2022-12-09 16:39:33 +0100

    start of texture work

--- src/draw3d.c
+++ src/draw3d.c
@@ -1,5 +1,12 @@
 #include "cub3D.h"
 
+static int     get_color_from_img(t_img img, int x, int y)
+{
+       if (x < 0 || y < 0 || x > WIN_W || y > WIN_H) //x > img.width || y > img.height)
+               return (0);
+       return (img->addr + (y * img->line_size + x * img->bits_per_pixel / 8));
+}
+
 void	draw3d(t_img *img, double dist, int x, int orientation) //need to explain name for rad_ang, why not current_angle or something else ?
 {
 	float	wall_bot;
@@ -14,5 +21,8 @@ void	draw3d(t_img *img, double dist, int x, int orientation) //need to explain n
 	if (wall_bot >= WIN_H)
 		wall_bot = WIN_H - 1;
 	while (--wall_bot > wall_top)
+	{
+		color = get_color_from_image(img, x, y);
 		put_pixel_img(img, x, (int)wall_bot, orientation);
+	}
 }
